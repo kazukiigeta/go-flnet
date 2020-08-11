76ed417a466cfc510ce746fafde7be9be4201e21
kazukiigeta <igetakazuki@gmail.com>
2020-08-11 09:42:23 -0400

    Add marshal functionality of trigger frame
    Add errcheck

--- messages.go
+++ messages.go
@@ -4,6 +4,10 @@
 
 package flnet
 
+import (
+	"io"
+)
+
 // FLnet is an interface that defines FL-net messages.
 type FLnet interface {
 	MarshalBinary() ([]byte, error)
@@ -23,8 +27,8 @@ func NewToken() *Token {
 		Header: NewFALinkHeader(
 			[4]uint8{0x46, 0x41, 0x43, 0x4e}, // H_TYPE
 			0x40,                             // TFL
-			0x1,                              // SA
-			0x55,                             // DA
+			0x1,                              // SNA
+			0x55,                             // DNA
 			0,                                // V_SEQ
 			0,                                // SEQ
 			false, false, false,              // M_CTL
@@ -37,7 +41,6 @@ func NewToken() *Token {
 			0, 3, true, 0x80, 0, // MODE, P_TYPE, PRI
 			1, 1, 0x40, // CBN, TBN, BSIZE
 			0, 0x32, 0, // LKS, TW, RCT
-			[]byte{},
 		),
 	}
 	return t
@@ -54,7 +57,7 @@ func (t *Token) MarshalBinary() ([]byte, error) {
 
 // MarshalLen returns the serial length of Token.
 func (t *Token) MarshalLen() int {
-	return 64
+	return t.Header.MarshalLen()
 }
 
 // UnmarshalBinary sets the values retrieved from byte sequence in a FL-net common header.
@@ -64,4 +67,91 @@ func (t *Token) UnmarshalBinary(b []byte) error {
 		return err
 	}
 	return nil
+}
+
+// ParticipationHeader is used for the commands of token participation.
+type ParticipationHeader struct {
+	Header  *FALinkHeader
+	NDN     [10]uint8
+	VDN     [10]uint8
+	MSN     [10]uint8
+	Reserve uint16
+}
+
+// Trigger is a trigger frame of FA Link frame.
+type Trigger ParticipationHeader
+
+// NewTrigger creates a new Trigger.
+func NewTrigger(sna, dna uint8, vseq, seq uint32, ndn, vdn, msn string) *Trigger {
+	phdr := &ParticipationHeader{}
+	phdr.Header = NewFALinkHeader(
+		[4]uint8{0x46, 0x41, 0x43, 0x4e}, // H_TYPE
+		0,                                // TFL
+		sna,                              // SNA
+		dna,                              // DNA
+		vseq,                             // V_SEQ
+		seq,                              // SEQ
+		false, false, false,              // M_CTL
+		0, 0, // ULS, M_SZ
+		0,          // M_ADD
+		0x0a, 0, 0, // MFT, M_RLT, reserved
+		TCDTrigger, 0, // TCD, VER
+		0, 0x04, // C_AD1, C_SZ1
+		0, 0x40, // C_AD2, C_SZ2
+		0, 3, true, 0x80, 0, // MODE, P_TYPE, PRI
+		1, 1, 0x40, // CBN, TBN, BSIZE
+		0, 0x32, 0, // LKS, TW, RCT
+	)
+	t := Trigger(*phdr)
+	t.Header.TFL = uint32(t.MarshalLen())
+	t.Header.BSize = uint16(t.MarshalLen())
+
+	t.NDN = [10]uint8{0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20}
+	t.VDN = [10]uint8{0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20}
+	t.MSN = [10]uint8{0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20}
+
+	copy(t.NDN[:len(ndn)], []uint8(ndn))
+	copy(t.VDN[:], []uint8(vdn))
+	copy(t.MSN[:], []uint8(msn))
+
+	return &t
+}
+
+// MarshalBinary returns the byte sequence generated from a Trigger.
+func (t *Trigger) MarshalBinary() ([]byte, error) {
+	b := make([]byte, t.MarshalLen())
+	if err := t.MarshalTo(b); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+//MarshalTo puts the byte sequence in the byte array given as b.
+func (t *Trigger) MarshalTo(b []byte) error {
+	l := len(b)
+	if l < t.MarshalLen() {
+		return io.ErrUnexpectedEOF
+	}
+
+	err := t.Header.MarshalTo(b)
+	if err != nil {
+		return err
+	}
+
+	offset := t.Header.MarshalLen()
+	copy(b[offset:], t.NDN[:])
+	offset += len(t.NDN)
+
+	copy(b[offset:], t.VDN[:])
+	offset += len(t.VDN)
+
+	copy(b[offset:], t.MSN[:])
+
+	return nil
+}
+
+// MarshalLen returns the serial length of Trigger
+func (t *Trigger) MarshalLen() int {
+	return 96
 }
